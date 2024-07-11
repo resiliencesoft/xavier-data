88535fc882c70f2691b553c741bb4d823c22c3cc
twinkledewangan <twinkledewangan@resiliencesoft.com>
2024-07-11 18:22:31 +0530

    done

--- src/app/Common/header/header.component.ts
+++ src/app/Common/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
- 
+  blue!: boolean;
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    console.log('blue')
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      const currentUrl = this.router.url;
+      
+       if (currentUrl === '/') {
+        console.log('Route is empty');
+        this.blue = false;
+        // Handle empty route case
+      }else{
+        console.log('hwllo')
+this.blue = true;
+console.log(this.blue)
+      }
+    });
+  }
 }
--- src/app/pages/description/description.component.ts
+++ src/app/pages/description/description.component.ts
@@ -7,5 +7,23 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
   styleUrl: './description.component.scss'
 })
 export class DescriptionComponent {
+  words: string[] = ['Alimentación', 'Bancos y seguros', 'Belleza y estética', 'Empresas de transporte', 'Servicios públicos', 'Servicios públicos', 'Alimentación', 'Belleza y estética', 'Alimentación', 'Bancos y seguros', 'Empresas de transporte', 'Servicios públicos'];
+  currentIndex: number = 0;
+  pageSize: number = 7;
+  
+  get currentWords(): string[] {
+    return this.words.slice(this.currentIndex, this.currentIndex + this.pageSize);
+  }
 
+  next(): void {
+    if (this.currentIndex + this.pageSize < this.words.length) {
+      this.currentIndex += 1;
+    }
+  }
+
+  previous(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex -= 1;
+    }
+  }
 }
